66558d70abbd4f74e87f25cb75de8a4a5e7b69ad
Cristian Carlassare <cristiancarlassare@MacBook-Pro-de-Cristian.local>
2025-03-02 19:17:44 -0300

    Initial commit

---
+++ wallbox-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wallbox-test
+//
+//  Created by Cristian Carlassare on 02/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wallbox-test/wallbox_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallbox_testApp.swift
+//  wallbox-test
+//
+//  Created by Cristian Carlassare on 02/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct wallbox_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
